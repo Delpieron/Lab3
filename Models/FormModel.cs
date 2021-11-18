2ee265dd95700dfc599a0c5e39eb7c09fe023891
krzys <krzys@DESKTOP-TKKASM2>
2021-11-18 21:19:04 +0100

    Add project files.

---
+++ Models/FormModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lab3_test2.Models
+{
+    public class FormModel
+    {
+        [Display(Name = "Imie")]
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Ilosc powtorzen")]
+        public int Iterations { get; set; }
+    }
+}
